7a4a4a887e874a7dea0151d919dd554a826dfe57
YuvarajV2479 <vyuvaraj@rvsgroup.com>
2024-02-28 10:39:40 +0530

    docs: update documentation

---
+++ src/commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      // You can add or customize rules here
+
+      'type-enum': [2, 'always', ['feat', 'fix', 'chore']],
+    },
+  };
+  
